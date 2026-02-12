e300f632ab9750c57b5cb0a99d531e3ef01e11e0
Kyle Quilty <kquilty@gmail.com>
2026-02-12 16:35:01 -0500

    Fixed preview/staging site not connecting to staging DB

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,36 +1,19 @@
 import { neon } from '@neondatabase/serverless';
 
-const appEnv = process.env.APP_ENV;
-const vercelEnv = process.env.VERCEL_ENV;
+const appEnv = process.env.NEXT_PUBLIC_ENV;
 
 function getDatabaseUrl() {
-
-// console.log(`App Environment: ${appEnv}`);
-// console.log(`Vercel Environment: ${vercelEnv}`);
-
-  if (vercelEnv === "production") {
+  if (appEnv === "production") {
     return process.env.DATABASE_URL;
   }
 
-  if (vercelEnv === "preview") {
-    return process.env.PREVIEW_DATABASE_URL ?? process.env.DATABASE_URL;
-  }
-
-  if (appEnv === "staging") {
-    return process.env.PREVIEW_DATABASE_URL;
-  }
-
-  if (process.env.NODE_ENV === "production") {
-    return process.env.DATABASE_URL;
-  }
-
-  return process.env.PREVIEW_DATABASE_URL ?? process.env.DATABASE_URL;
+  return process.env.PREVIEW_DATABASE_URL;
 }
 
 const targetURL = getDatabaseUrl();
 
 if (!targetURL) {
-  throw new Error('Unable to determine the database URL. DATABASE_URL or PREVIEW_DATABASE_URL is not set up.');
+  throw new Error('Unable to determine the database URL. PREVIEW_DATABASE_URL is not set up for non-production, or DATABASE_URL is missing for production.');
 }
 
 export const sql = neon(targetURL);
